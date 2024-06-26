d9e2484c82d26254a02c3ce5c57f5f49a56e0767
Guillaume Destree <devstree.guillaume@gmail.com>
2024-06-26 10:51:09 +0200

    essai correction download

--- src/components/BtnCv.jsx
+++ src/components/BtnCv.jsx
@@ -3,7 +3,7 @@ import "../styles/BtnCv.scss"
 const BtnCv = () => {
   return (
     <div className="btnCv">
-      <a href="../../public/Destree_guillaumeCV.pdf">
+      <a href="../../public/Destree_guillaumeCV.pdf" download="Destree_guillaumeCV.pdf">
         <button type="button">
           Téléchargez mon CV !
         </button>
