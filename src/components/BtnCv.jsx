d8b2a878f741da812faf04ce1f796a83dbb9ac10
Guillaume Destree <devstree.guillaume@gmail.com>
2024-06-26 11:23:08 +0200

    essai avec style

--- src/components/BtnCv.jsx
+++ src/components/BtnCv.jsx
@@ -1,14 +1,11 @@
 import "../styles/BtnCv.scss";
-import Cv from "../../public/destree_guillaume_cv.pdf"
+import Cv from "../../public/destree_guillaume_cv.pdf";
 
 const BtnCv = () => {
   return (
     <div className="btnCv">
-      <a
-        href={Cv}
-        download={Cv}
-      >
-        Téléchargez mon CV !
+      <a href={Cv} download>
+        <button>Téléchargez mon CV !</button>
       </a>
     </div>
   );
