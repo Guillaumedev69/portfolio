f9e8efe0538f7e7186d8e8925b76a891d0299742
Guillaume Destree <devstree.guillaume@gmail.com>
2024-06-26 11:13:06 +0200

    supression btn en laissant un link

--- src/components/BtnCv.jsx
+++ src/components/BtnCv.jsx
@@ -1,12 +1,13 @@
-import "../styles/BtnCv.scss"
+import "../styles/BtnCv.scss";
 
 const BtnCv = () => {
   return (
     <div className="btnCv">
-      <a href="../../public/destree_guillaume_cv.pdf" download="destree_guillaume_cv.pdf">
-        <button type="button">
-          Téléchargez mon CV !
-        </button>
+      <a
+        href="../../public/destree_guillaume_cv.pdf"
+        download="destree_guillaume_cv.pdf"
+      >
+        Téléchargez mon CV !
       </a>
     </div>
   );
