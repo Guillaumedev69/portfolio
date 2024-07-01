0aff68d844a4aa0fd7df19a4bc131f6c982cc188
Guillaume Destree <devstree.guillaume@gmail.com>
2024-07-01 17:34:26 +0200

    modification animation skills et background main

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,4 @@
-import "../styles/Skills.scss"
+import "../styles/Skills.scss";
 import IconReact from "../assets/icons/react-icon.svg";
 import IconSass from "../assets/icons/sass-icon.svg";
 import IconJavascript from "../assets/icons/javascript-icon.svg";
@@ -8,80 +8,77 @@ import IconNode from "../assets/icons/node-icon.svg";
 import IconMongo from "../assets/icons/mongo-icon.svg";
 import IconFigma from "../assets/icons/figma-icon.svg";
 
-
 const Skills = () => {
-    return (
-        <div>
-            <div className="skillsContainer">
-          <div className="skillsContainer__iconsTitleContain">
-            <img
-              className="skillsContainer__icons"
-              src={IconReact}
-              alt="logo du framework REACT"
-            />
-            <h3 className="skillsContainer__title">React</h3>
-          </div>
-          <div className="skillsContainer__iconsTitleContain">
-            <img
-              className="skillsContainer__icons"
-              src={IconSass}
-              alt="logo de Sass"
-            />
-            <h3 className="skillsContainer__title">Sass</h3>
-          </div>
-          <div className="skillsContainer__iconsTitleContain">
-            <img
-              className="skillsContainer__icons"
-              src={IconMongo}
-              alt="logo de la base de données MongoDB"
-            />
-            <h3 className="skillsContainer__title">MongoDB</h3>
-          </div>
-          <div className="skillsContainer__iconsTitleContain">
-            <img
-              className="skillsContainer__icons"
-              src={IconNode}
-              alt="logo du langage Node.js"
-            />
-            <h3 className="skillsContainer__title">Node.js</h3>
-          </div>
-          <div className="skillsContainer__iconsTitleContain">
-            {" "}
-            <img
-              className="skillsContainer__icons"
-              src={IconJavascript}
-              alt="logo du langage JavaScript"
-            />
-            <h3 className="skillsContainer__title">JavaScript</h3>
-          </div>
-          <div className="skillsContainer__iconsTitleContain">
-            {" "}
-            <img
-              className="skillsContainer__icons"
-              src={IconCss}
-              alt="logo du langage de style CSS"
-            />
-            <h3 className="skillsContainer__title">CSS 3</h3>
-          </div>
-          <div className="skillsContainer__iconsTitleContain">
-            <img
-              className="skillsContainer__icons"
-              src={IconHtml}
-              alt="logo du langage HTML"
-            />
-            <h3 className="skillsContainer__title">HTML 5</h3>
-          </div>
-          <div className="skillsContainer__iconsTitleContain">
-            <img
-              className="skillsContainer__icons"
-              src={IconFigma}
-              alt="icone du logiciel de design Figma"
-            />
-            <h3 className="skillsContainer__title">Figma</h3>
-          </div>
+  return (
+    <div>
+      <div className="skillsContainer">
+        <div className="skillsContainer__iconsTitleContain">
+          <img
+            className="skillsContainer__icons"
+            src={IconReact}
+            alt="logo du framework REACT"
+          />
+          <h3 className="skillsContainer__title">React</h3>
         </div>
+        <div className="skillsContainer__iconsTitleContain">
+          <img
+            className="skillsContainer__icons"
+            src={IconSass}
+            alt="logo de Sass"
+          />
+          <h3 className="skillsContainer__title">Sass</h3>
         </div>
-    );
+        <div className="skillsContainer__iconsTitleContain">
+          <img
+            className="skillsContainer__icons"
+            src={IconMongo}
+            alt="logo de la base de données MongoDB"
+          />
+          <h3 className="skillsContainer__title">MongoDB</h3>
+        </div>
+        <div className="skillsContainer__iconsTitleContain">
+          <img
+            className="skillsContainer__icons"
+            src={IconNode}
+            alt="logo du langage Node.js"
+          />
+          <h3 className="skillsContainer__title">Node.js</h3>
+        </div>
+        <div className="skillsContainer__iconsTitleContain">
+          <img
+            className="skillsContainer__icons"
+            src={IconJavascript}
+            alt="logo du langage JavaScript"
+          />
+          <h3 className="skillsContainer__title">JavaScript</h3>
+        </div>
+        <div className="skillsContainer__iconsTitleContain">
+          <img
+            className="skillsContainer__icons"
+            src={IconCss}
+            alt="logo du langage de style CSS"
+          />
+          <h3 className="skillsContainer__title">CSS 3</h3>
+        </div>
+        <div className="skillsContainer__iconsTitleContain">
+          <img
+            className="skillsContainer__icons"
+            src={IconHtml}
+            alt="logo du langage HTML"
+          />
+          <h3 className="skillsContainer__title">HTML 5</h3>
+        </div>
+        <div className="skillsContainer__iconsTitleContain">
+          <img
+            className="skillsContainer__icons"
+            src={IconFigma}
+            alt="icone du logiciel de design Figma"
+          />
+          <h3 className="skillsContainer__title">Figma</h3>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
