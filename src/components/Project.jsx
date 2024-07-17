50914425cbd3a2581407892bdde50dec4cc954cf
Guillaume Destree <devstree.guillaume@gmail.com>
2024-07-17 14:59:01 +0200

    Maj cards

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,5 @@
-import {
-  FlippingCard,
-  FlippingCardBack,
-  FlippingCardFront,
-} from "react-ui-cards";
 import "../styles/Project.scss";
+import { useState } from "react";
 import ImgKasa from "../assets/imgKasa.webp";
 import ImgSophieBluel from "../assets/imgSophieBluel.webp";
 import ImgBooki from "../assets/imgBooki.webp";
@@ -19,161 +15,108 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
 const Project = () => {
+  const [flippedCards, setFlippedCards] = useState([]);
+
+  const handleCardClick = (index) => {
+    setFlippedCards((prevFlippedCards) => {
+      const isAlreadyFlipped = prevFlippedCards.includes(index);
+      if (isAlreadyFlipped) {
+        return prevFlippedCards.filter((cardIndex) => cardIndex !== index);
+      } else {
+        return [...prevFlippedCards, index];
+      }
+    });
+  };
+
+  const projects = [
+    {
+      img: ImgKasa,
+      title: "Kasa - Location immobilière",
+      description: `Mon projet sur l'application web Kasa était l'intégration
+        d'une maquette Figma en React & SASS.
+        J'ai dû créer et implémenter plusieurs pages et les lier
+        grâce à React-Router-Dom, créer des composant réutilisables.
+        Le plus difficile à gérer c'était l'affichage de la page
+        404 pour une erreure d'ID de cards et de créer des style
+        different pour le même composant comme les collaspes.`,
+      icons: [IconReact, IconSass],
+      link: "https://github.com/Guillaumedev69/Kasa-Fr",
+    },
+    {
+      img: ImgSophieBluel,
+      title: "Sophie Bluel - Portfolio en ligne",
+      description: `Le sujet principal de ce projet était l'intégration d'une
+        maquette Figma avec JavaScript & CSS.
+        Dans la maquette j'avais des modales à créer avec une
+        liaison vers l'API CRUD pour que le client puisse
+        Ajouter, Afficher, Modifier ou Supprimer des projets de sa galerie.
+        Ce m'a donné quelques difficultées pour mes premiers appels API
+        avec Fetch et pour la création de modale.`,
+      icons: [IconJavascript, IconCss],
+      link: "https://github.com/Guillaumedev69/Portfolio_Sophie_Bluel",
+    },
+    {
+      img: ImgBooki,
+      title: "Booki - Agence de voyage",
+      description: `Booki étant mon premier projet je devais integrer une maquette figma
+        en utilisant uniquement HTML et CSS.
+        Mes difficultées sur ce projet ont été le responsive design et les
+        petits details de la maquette qui étaient différent suivant les
+        tailles d'écrans.`,
+      icons: [IconHtml, IconCss],
+      link: "https://github.com/Guillaumedev69/Booki_integration",
+    },
+    {
+      img: ImgMonVieuxGrimoire,
+      title: "Mon Vieux Grimoire - librairie",
+      description: `Mon Vieux Grimoire et une application web en React qui permet aux
+        utilisateurs de s'inscrire et/ou de se connecter pour ajouter
+        des livres ne ligne et de les noter.
+        Ma mission était de créer la partie serveur avec une base de données
+        (CRUD) pour que les clients puissent
+        Ajouter, Afficher, Modifier ou Supprimer les livres ajoutées.
+        Pour moi les points les plus compliqué ont été de gérer l'ajout
+        et le traitement des images avec multer et Sharp.`,
+      icons: [IconNode, IconMongo],
+      link: "https://github.com/Guillaumedev69/Mon-Vieux-Grimoire",
+    },
+  ];
+
   return (
     <div className="containerCards">
-      <FlippingCard className="cards">
-        <FlippingCardBack>
-          <p className="cards__P">
-            Mon projet sur l&apos;application web Kasa était l&apos;intégration
-            d&apos;une maquette Figma en React & SASS.
-            <br /> J&apos;ai dû créer et implémenter plusieurs pages et les lier
-            grâce à React-Router-Dom, créer des composant réutilisables.
-            <br />
-            Le plus difficile à gérer c&apos;était l&apos;affichage de la page
-            404 pour une erreure d&apos;ID de cards et de créer des style
-            different pour le même composant comme les collaspes.
-          </p>
-          <div className="cardsIconsContainer">
+      {projects.map((project, index) => (
+        <div
+          className={`cards ${flippedCards.includes(index) ? "flipped" : ""}`}
+          key={index}
+          onClick={() => handleCardClick(index)}
+        >
+          <div className="cards__frontContent">
             <img
-              className="cardsIcons"
-              src={IconReact}
-              alt="logo du framework REACT"
+              className="cards__Img"
+              src={project.img}
+              alt={`capture d'écran du projet ${project.title}`}
             />
-            <img className="cardsIcons" src={IconSass} alt="logo de Sass" />
-            <a href="https://github.com/Guillaumedev69/Kasa-Fr" target="blank">
-              <FontAwesomeIcon icon={faArrowUpRightFromSquare} /> Voir plus
-            </a>
+            <h3 className="cards__Title">{project.title}</h3>
           </div>
-        </FlippingCardBack>
-        <FlippingCardFront>
-          <img
-            className="cards__Img"
-            src={ImgKasa}
-            alt="capture d'écran du projet Kasa"
-          />
-          <h3 className="cards__Title">Kasa - Location immobilière</h3>
-        </FlippingCardFront>
-      </FlippingCard>
-      <FlippingCard className="cards">
-        <FlippingCardBack>
-          <p className="cards__P">
-            Le sujet principal de ce projet était l&apos;intégration d&apos;une
-            maquette Figma avec JavaScript & CSS.
-            <br /> Dans la maquette j&apos;avais des modales à créer avec une
-            liaison vers l&apos;API CRUD pour que le client puisse
-            Ajouter/Afficher/Modifier/Supprimer des projets de sa galerie.
-            <br />
-            Ce m&apos;a donné quelques difficultées pour mes premiers appels API
-            avce Fetch et pour la création de modale.
-          </p>
-          <div className="cardsIconsContainer">
-            <img
-              className="cardsIcons"
-              src={IconJavascript}
-              alt="logo du langage JavaScript"
-            />
-            <img
-              className="cardsIcons"
-              src={IconCss}
-              alt="logo du langage de style CSS"
-            />
-            <a
-              href="https://github.com/Guillaumedev69/Portfolio_Sophie_Bluel"
-              target="blank"
-            >
+          <div className="cards__backContent">
+            <p className="cards__P">{project.description}</p>
+            <div className="cardsIconsContainer">
+              {project.icons.map((icon, iconIndex) => (
+                <img
+                  className="cardsIcons"
+                  src={icon}
+                  alt="project icon"
+                  key={iconIndex}
+                />
+              ))}
+              <div><a href={project.link} target="blank">
               <FontAwesomeIcon icon={faArrowUpRightFromSquare} /> Voir plus
-            </a>
+            </a></div>
+            </div>
+            
           </div>
-        </FlippingCardBack>
-        <FlippingCardFront>
-          <img
-            className="cards__Img"
-            src={ImgSophieBluel}
-            alt="capture d'écran du projet Sophie Bluel"
-          />
-          <h3 className="cards__Title">Sophie Bluel - Portfolio en ligne</h3>
-        </FlippingCardFront>
-      </FlippingCard>
-      <FlippingCard className="cards">
-        <FlippingCardBack>
-          <p className="cards__P">
-            Booki étant mon premier projet je devais integrer une maquette figma
-            en utilisant uniquement HTML et CSS.
-            <br />
-            Mes difficultées sur ce projet ont été le responsive design et les
-            petits details de la maquette qui étaient différent suivant les
-            tailles d&apos;écrans.
-          </p>
-          <div className="cardsIconsContainer">
-            <img
-              className="cardsIcons"
-              src={IconHtml}
-              alt="logo du langage HTML"
-            />
-            <img
-              className="cardsIcons"
-              src={IconCss}
-              alt="logo du langage de style CSS"
-            />
-            <a
-              href="https://github.com/Guillaumedev69/Booki_integration"
-              target="blank"
-            >
-              <FontAwesomeIcon icon={faArrowUpRightFromSquare} /> Voir plus
-            </a>
-          </div>
-        </FlippingCardBack>
-        <FlippingCardFront>
-          <img
-            className="cards__Img"
-            src={ImgBooki}
-            alt="capture d'écran du projet Booki"
-          />
-          <h3 className="cards__Title">Booki - Agence de voyage</h3>
-        </FlippingCardFront>
-      </FlippingCard>
-      <FlippingCard className="cards">
-        <FlippingCardBack>
-          <p className="cards__P">
-            Mon Vieux Grimoire et une application web en React qui permet aux
-            utilisateurs de s&apos;inscrire et/ou de se connecter pour ajouter
-            des livres ne ligne et de les noter.
-            <br />
-            Ma mission était de créer la partie serveur avec une base de données
-            (CRUD) pour que les clients puissent
-            Ajouter/Afficher/Modifier/supprimer les livres ajoutées.
-            <br />
-            Pour moi les points les plus compliqué ont été de gérer l&apos;ajout
-            et le traitement des images avec multer et Sharp.
-          </p>
-          <div className="cardsIconsContainer">
-            <img
-              className="cardsIcons"
-              src={IconNode}
-              alt="logo du langage Node.js"
-            />
-            <img
-              className="cardsIcons"
-              src={IconMongo}
-              alt="logo de la base de données MongoDB"
-            />
-            <a
-              href="https://github.com/Guillaumedev69/Mon-Vieux-Grimoire"
-              target="blank"
-            >
-              <FontAwesomeIcon icon={faArrowUpRightFromSquare} /> Voir plus
-            </a>
-          </div>
-        </FlippingCardBack>
-        <FlippingCardFront>
-          <img
-            className="cards__Img"
-            src={ImgMonVieuxGrimoire}
-            alt="capture d'écran du projet Mon Vieux Grimoire"
-          />
-          <h3 className="cards__Title">Mon Vieux Grimoire - librairie</h3>
-        </FlippingCardFront>
-      </FlippingCard>
+        </div>
+      ))}
     </div>
   );
 };
